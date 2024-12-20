b29cb9ebc7a40a64a4ecbc9569baeacd0a99b9f1
maathbluee <mathys.cunsolo@gmail.com>
2024-12-20 11:17:58 +0100

    added token verification for users

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const verifyToken = require('../middlewares/verifyToken');
 const { validateUserCreation } = require('../middlewares/validateUserCreation');
 const { validateUserLogin } = require('../middlewares/validateUserLogin');
 
