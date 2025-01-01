68f44180f03945fdd75d3f5af4bb30979e87d33a
MaathBluee <mathys.cunsolo@gmail.com>
2025-01-01 15:44:54 +0100

    added edition validatons for restaurants

---
+++ server/middlewares/validateRestaurantEdition.js
@@ -0,0 +1,37 @@
+const { check, validationResult } = require('express-validator');
+
+const validateRestaurantEdition = [
+    check('name')
+        .optional()
+        .isLength({ max: 100 }).withMessage('Le nom ne doit pas dépasser 100 caractères.'),
+
+    check('address')
+        .optional()
+        .isLength({ max: 255 }).withMessage('L\'adresse ne doit pas dépasser 255 caractères.'),
+
+    check('postalCode')
+        .optional()
+        .isPostalCode('FR').withMessage('Le code postal doit être valide pour la France.'),
+
+    check('city')
+        .optional()
+        .isLength({ max: 100 }).withMessage('La ville ne doit pas dépasser 100 caractères.'),
+
+    check('email')
+        .optional()
+        .isEmail().withMessage('L\'email doit être valide.'),
+
+    check('password')
+        .optional()
+        .isLength({ min: 8 }).withMessage('Le mot de passe doit contenir au moins 8 caractères.'),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
+];
+
+module.exports = { validateRestaurantEdition };
--- server/routes/restaurantRoutes.js
+++ server/routes/restaurantRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const verifyToken = require('../middlewares/verifyToken');
 const checkRole = require('../middlewares/checkRole');
 const { validateRestaurantCreation } = require('../middlewares/validateRestaurantCreation');
+const { validateRestaurantEdition } = require('../middlewares/validateRestaurantEdition');
 const restaurantController = require('../controllers/restaurantController');
 
 router.get('/', verifyToken, checkRole(['admin']), restaurantController.getAllRestaurants);
 router.post('/create', verifyToken, checkRole(['admin']), validateRestaurantCreation, restaurantController.createRestaurant);
-router.put('/:id', verifyToken, checkRole(['admin', 'restaurant']), restaurantController.editRestaurant);
+router.put('/:id', verifyToken, checkRole(['admin', 'restaurant']), validateRestaurantEdition, restaurantController.editRestaurant);
 router.delete('/:id', verifyToken, checkRole(['admin']), restaurantController.deleteRestaurant);
 
 module.exports = router;
